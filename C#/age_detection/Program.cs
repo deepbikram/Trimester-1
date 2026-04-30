7b515d904481988cb44565ec56eae8363797bf94
Deep Bikram Thapa Chhetri <deepbikram01@gmail.com>
2026-04-30 08:05:36 +0545

    age detection andd grading system

---
+++ C#/age_detection/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age >= 0 && age < 18) {
+            Console.WriteLine("Go to study.");
+        } else if (age < 55 && age >= 18) {
+            Console.WriteLine("You are allowed to party");
+        } else if (age > 55 && age < 125) {
+            Console.WriteLine("You, go to sleep.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid age.");
+        }
+    }
+}
